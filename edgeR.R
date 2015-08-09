a87b4b9ac55278c846a1a2b11b7b3739f8c906c7
tirohia <ben@tirohia.net>
2015-08-09 16:45:12 +1200

    Removed the data loading functions to tools.R
    These are now called as a function
    
    xxxanalysis(counts,subject,track,time1,time2,outputImages,outputDir)

--- edgeR.R
+++ edgeR.R
@@ -1,159 +1,130 @@
-library("edgeR")
-library(RColorBrewer)
+require("edgeR")
+require(RColorBrewer)
+require(gplots)
 
-subject<-"bacteria"  #should be plant or psa
-track<-"psa"  #should be psa or control, i.e. treatment
-
-outputDir<-"/home/ben/Google Drive/PhD/writing/Timecourse/images/"
-
-setwd("/home/ben/workspace/timeCourse/data")
-source("../code/R/tools.R")
-if (subject=="plant"){
-  datafile="/home/ben/workspace/timeCourse/data/actinidia/cornellGenomeModelTH-TCFrequencyMatrix.csv"
-}else if (subject=="bacteria"){
-  datafile="/home/ben/workspace/timeCourse/data/psa/psaFrequencyMatrix.csv"
-}else {
-  print ("please specify subject <- plant or bacteria")
-}
-
-
-## load data into rawCounts
-counts = read.table( datafile,sep=",", header=TRUE, row.names=1,stringsAsFactors = FALSE )
-counts <-cleanDataNames(counts)
-design <- getDataRange(counts,1.5,120,track)
-datacolumns<-getDataCols(design)
-counts<-counts[,datacolumns]
-
-if (subject=="bacteria"){
-  ## remove ribosomal entries from psa data. 
-  ribosomalEntriesfile="/home/ben/workspace/models/psa/ribosomalIYO"
-  ribosomalEntries = read.table( ribosomalEntriesfile,sep=",", header=FALSE,stringsAsFactors = FALSE )
-  counts<-counts[ ! rownames(counts) %in% ribosomalEntries, ] 
-  counts <- head(counts, -5)
-  #tail(counts)
-}
-
+## data should be loaded elsewhere using load.data() from tools.R
 
 ####################
 ### glm. 
 
-
-#condition=as.factor(c(rep("control",each=15),rep("treated",each=15)))
-condition=as.factor(rep("treated",each=27))
-
-time=c(1,2,3,4,5,6,7,8,9)
-time=as.factor(c(rep(time,each=3)))
-
-targets <- data.frame(condition=condition,time=time)
+edgeRanalysis<-function(counts,subject,track,time1,time2,outputImages,outputDir){
+  #condition=as.factor(c(rep("control",each=15),rep("treated",each=15)))
+  if (track=="all"){
+    design <- getDataRange(time1,time2)
+  }else{
+    design <- getDataRange(time1,time2,track)
+  }
+  design <- getDataRange(0,120,"psa")
+  timeList<-design$time
+  numberOfPoints<-length(design$time)
+  condition=as.factor(rep("treated",each=numberOfPoints*3))
+  time=c(1:length(design$time))
+  time=as.factor(c(rep(time,each=3)))
+  targets <- data.frame(condition=condition,time=time)
+  
 
 
 #Group <- as.factor(paste(targets$condition,targets$time,sep="."))
 #targets<-cbind(targets,Group=Group)
 
-y <- DGEList(counts=counts,group=time)
-design <- model.matrix(~time)
-y <- estimateGLMCommonDisp(y,design)
-y <- estimateGLMTrendedDisp(y,design)
-y <- estimateGLMTagwiseDisp(y,design)
-fit <- glmFit(y,design)
+  y <- DGEList(counts=counts,group=time)
+  design <- model.matrix(~time)
+  y <- estimateGLMCommonDisp(y,design)
+  y <- estimateGLMTrendedDisp(y,design)
+  y <- estimateGLMTagwiseDisp(y,design)
+  fit <- glmFit(y,design)
 
 #targets$condition <- relevel(targets$condition, ref="treated")
 #design <- model.matrix(~condition + time, data=targets)
 
-total<-vector('character')  #for unique genes
-geneMap<-matrix(data=0,nrow=9,ncol=9)
-rownames(geneMap)<-paste("time",c(1:9),sep="")
-colnames(geneMap)<-paste("time",c(1:9),sep="")
+  total<-vector('character')  #for unique genes
+  geneMap<-matrix(data=0,nrow=numberOfPoints,ncol=numberOfPoints)
+  rownames(geneMap)<-paste("time",c(1:numberOfPoints),sep="")
+  colnames(geneMap)<-paste("time",c(1:numberOfPoints),sep="")
 
 
-for (i in c(1:9) ){
-  for (j in c(1:9)){
-    if(i==j){
-      geneMap[paste("time",i,sep=""),paste("time",j,sep="")]<-0
-      next
-    }
-    a = c(0,0,0,0,0,0,0,0,0)
-    if (i==1){
-      a<-replace(a,j,1)
-    }
-    else if (j==1){
-      a<-replace(a,i,-1)
-    }
-    else{
-      a<-replace(a,c(i,j),c(1,-1))
-    }
-    lrt <- glmLRT(fit, contrast=a)
-    tags<-as.data.frame(topTags(lrt,39039,sort.by="logFC"))
-    tags<- tags[tags$FDR < 0.05,]
-    tagsUp<- tags[tags$logFC > 2,]
-    tagsDown<- tags[tags$logFC < -2,]
-    total<-c(total,rownames(tagsUp))
-    write.csv(rownames(tagsUp),paste("/home/ben/workspace/timeCourse/data/deResults/edgeR/edgeRpsa",i,"-",j,"up.csv",sep=""),row.names=FALSE)
-    write.csv(rownames(tagsDown),paste("/home/ben/workspace/timeCourse/data/deResults/edgeR/edgeRpsa",i,"-",j,"down.csv",sep=""),row.names=FALSE)
-    
-    if (is.integer(nrow(tagsUp))){
-      geneMap[paste("time",i,sep=""),paste("time",j,sep="")]<-nrow(tagsUp)
-    }
-    
-  }  
-}
+  for (i in c(1:numberOfPoints) ){
+    for (j in c(1:numberOfPoints)){
+      if(i==j){
+        geneMap[paste("time",i,sep=""),paste("time",j,sep="")]<-0
+        next
+      }
+      a = c(rep(0,each=numberOfPoints))
+      if (i==1){
+        a<-replace(a,j,1)
+      }
+      else if (j==1){
+        a<-replace(a,i,-1)
+      }
+      else{
+        a<-replace(a,c(i,j),c(1,-1))
+      }
+      lrt <- glmLRT(fit, contrast=a)
+      tags<-as.data.frame(topTags(lrt,39039,sort.by="logFC"))
+      tags<- tags[tags$FDR < 0.05,]
+      tagsUp<- tags[tags$logFC > 2,]
+      tagsDown<- tags[tags$logFC < -2,]
+      total<-c(total,rownames(tagsUp))
+      if(!is.null(outputDir)){
+        if(!dir.exists(file.path(outputDir))){
+          dir.create(file.path(mainDir, subDir), showWarnings = FALSE)
+        }
+        write.csv(rownames(topUp),paste(outputDir,"/edgeR",subject,"-",track,i,"-",j,"up.csv",sep=""),row.names=FALSE)
+        write.csv(rownames(topDown),paste(outputDir,"/edgeR",subject,"-",track,i,"-",j,"down.csv",sep=""),row.names=FALSE)
+      }
+      if (is.integer(nrow(tagsUp))){
+        geneMap[paste("time",i,sep=""),paste("time",j,sep="")]<-nrow(tagsUp)
+      }
+    }  
+  }
 
 ###
 
 #get number of unique genes. 
+  if(!is.null(outputImages)){
+    if(subject=="plant" && track=="control"){
+      filenameUp = paste(outputImages,"edgeRGeneMapControlUp",time1,"-",time2,".png",sep="")
+      filenameDown = paste(outputImages,"edgeRGeneMapControlDown",time1,"-",time2,".png",sep="")
+      mainUp<-paste("Actinidia, control - up",sep="")
+      mainDown<-paste("Actinidia, control - down",sep="")
+      palette<-brewer.pal(9,name="Greens")
+    }else if (subject=="plant" && track=="psa"){
+      filenameUp = paste(outputImages,"edgeRGeneMapInnoculatedUp",time1,"-",time2,".png",sep="")
+      filenameDown = paste(outputImages,"edgeRGeneMapInnoculatedDown",time1,"-",time2,".png",sep="")
+      mainUp<-paste("Actinidia, innoculated - up",sep="")
+      mainDown<-paste("Actinidia, innoculated - down",sep="")
+      palette<-brewer.pal(9,name ="Blues")
+    }else if (subject=="bacteria" && track=="psa"){
+      filenameUp = paste(outputImages,"edgeRGeneMapPsaUp",time1,"-",time2,".png",sep="")
+      filenameDown = paste(outputImages,"edgeRGeneMapPsaDown",time1,"-",time2,".png",sep="")
+      mainUp<-paste("Psa, bacterial genes - up",sep="")
+      mainDown<-paste("Psa, bacterial genes - down",sep="")
+      palette<-brewer.pal(9,name="Reds")
+    }else{
+      print("there should have been an error before here but either the plant/bacteria or psa/control thing hasn't been specified")
+    }
 
-###
+    colnames(geneMap)<-timeList
+    rownames(geneMap)<-timeList
 
+    matUp<-geneMap
+    matUp[upper.tri(geneMap)]<-0
+    png(filename=filenameUp,width=800)
+    heatmap.2( log(matUp+1),scale="none",col = palette,margins = c(5, 15),trace = "none",lhei = c(0.75,3),xlab = "Hours post innoculation", na.color="white", main = mainUp ,Colv = FALSE,Rowv =FALSE, dendrogram = "none")
+    dev.off()
 
-if(subject=="plant" && track=="control"){
-  filenameUp = paste(outputDir,"edgeRGeneMapControlUp.png",sep="")
-  filenameDown = paste(outputDir,"edgeRGeneMapControlDown.png",sep="")
-  mainUp<-paste("Actinidia, control - up",sep="")
-  mainDown<-paste("Actinidia, control - down",sep="")
-  palette<-brewer.pal(9,name="Greens")
-}else if (subject=="plant" && track=="psa"){
-  filenameUp = paste(outputDir,"edgeRGeneMapInnoculatedUp.png",sep="")
-  filenameDown = paste(outputDir,"edgeRGeneMapInnoculatedDown.png",sep="")
-  mainUp<-paste("Actinidia, innoculated - up",sep="")
-  mainDown<-paste("Actinidia, innoculated - down",sep="")
-  palette<-brewer.pal(9,name ="Blues")
-}else if (subject=="bacteria" && track=="psa"){
-  filenameUp = paste(outputDir,"edgeRGeneMapPsaUp.png",sep="")
-  filenameDown = paste(outputDir,"edgeRGeneMapPsaDown.png",sep="")
-  mainUp<-paste("Psa, bacterial genes - up",sep="")
-  mainDown<-paste("Psa, bacterial genes - down",sep="")
-  palette<-brewer.pal(9,name="Reds")
-}else{
-  print("there should have been an error before here but either the plant/bacteria or psa/control thing hasn't been specified")
+    matDown<-geneMap
+    matDown[lower.tri(geneMap)]<-0
+    png(filename=filenameDown,width=800)
+    heatmap.2( log(matDown+1),scale="none",col = palette,margins = c(5, 15),trace = "none",lhei = c(0.75,3),xlab = "Hours post innoculation", na.color="white", main = mainDown,Colv = FALSE,Rowv =FALSE, dendrogram = "none")
+    dev.off()
+  }
+  print("total number of gene changes:")
+  print(length(total))
+  print("total number of unique genes changing:")
+  print(length(unique(total)))
+
 }
-
-colnames(geneMap)<-c(1.5,3,6,12,24,48,72,96,120)
-rownames(geneMap)<-c(1.5,3,6,12,24,48,72,96,120)
-
-matUp<-geneMap
-matUp[upper.tri(geneMap)]<-0
-png(filename=filenameUp,width=800)
-heatmap.2( log(matUp+1),scale="none",col = palette,margins = c(5, 15),trace = "none",lhei = c(0.75,3),xlab = "Comparison", na.color="white", main = mainUp ,Colv = FALSE,Rowv =FALSE, dendrogram = "none")
-dev.off()
-
-
-
-#heatmap.2( log(matUp+1),scale="none",col = palette,margins = c(5, 15),trace = "none",lhei = c(0.75,3),xlab = "Comparison", na.color="white", main = mainUp ,Colv = FALSE,Rowv =FALSE, dendrogram = "none")
-
-matDown<-geneMap
-matDown[lower.tri(geneMap)]<-0
-png(filename=filenameDown,width=800)
-heatmap.2( log(matDown+1),scale="none",col = palette,margins = c(5, 15),trace = "none",lhei = c(0.75,3),xlab = "Comparison", na.color="white", main = mainDown,Colv = FALSE,Rowv =FALSE, dendrogram = "none")
-dev.off()
-
-
-print("total number of gene changes:")
-print(length(total))
-print("total number of unique genes changing:")
-print(length(unique(total)))
-
-
-
 ###################################
 
 #design <- model.matrix(~condition * time, data=targets)
--- voomlimma.R
+++ voomlimma.R
@@ -2,153 +2,129 @@
 library("limma")
 library("Biobase")
 library("gplots")
+library(gtools)
 library(RColorBrewer)
 
-subject<-"bacteria"  #should be plant or bacteria
-track<-"psa"  #should be psa or control, i.e. treatment
 
-outputDir<-"/home/ben/Google Drive/PhD/writing/Timecourse/images/"
-
-
-setwd("/home/ben/workspace/timeCourse/data")
-source("../code/R/tools.R")
-if (subject=="plant"){
-  datafile="/home/ben/workspace/timeCourse/data/actinidia/cornellGenomeModelTH-TCFrequencyMatrix.csv"
-}else if (subject=="bacteria"){
-  datafile="/home/ben/workspace/timeCourse/data/psa/psaFrequencyMatrix.csv"
-}else {
-  print ("please specify subject <- plant or bacteria")
-}
-
-
-## load data into counts
-counts = read.table( datafile,sep=",", header=TRUE, row.names=1,stringsAsFactors = FALSE )
-counts <-cleanDataNames(counts)
-design <- getDataRange(counts,1.5,120,track)
-datacolumns<-getDataCols(design)
-counts<-counts[,datacolumns]
-
-if (subject=="psa"){
-## remove ribosomal entries from psa data. 
-  ribosomalEntriesfile="/home/ben/workspace/models/psa/ribosomalIYO"
-  ribosomalEntries = read.table( ribosomalEntriesfile,sep=",", header=FALSE,stringsAsFactors = FALSE )
-  counts<-counts[ ! rownames(counts) %in% ribosomalEntries, ] 
-  counts <- head(counts, -5)
-  #tail(counts)
-}
+limmaAnalysis<-function(counts,subject,track,time1,time2,outputImages,outputDir){
+  #condition=as.factor(c(rep("control",each=15),rep("treated",each=15)))
+  if (track=="all"){
+    design <- getDataRange(time1,time2)
+  }else{
+    design <- getDataRange(time1,time2,track)
+  }
+  design <- getDataRange(time1,time2,"psa")
+  design
+  timeList<-design$time
+  numberOfPoints<-length(design$time)
+  condition=as.factor(rep("treated",each=numberOfPoints*3))
+  time=c(1:length(design$time))
+  time=as.factor(c(rep(time,each=3)))
+  targets <- data.frame(condition=condition,time=time)
 
 ####################
 ### glm. 
 
-
-time=c("s1","s2","s3","s4","s5","s6","s7","s8","s9")
-time=as.factor(c(rep(time,each=3)))
-design <- model.matrix(~0+time)
-colnames(design) <- c("s1","s2","s3","s4","s5","s6","s7","s8","s9")
-
-
-dge <- DGEList(counts=counts)
-dge <- calcNormFactors(dge)
-v <- voom(dge,design,plot=TRUE)
-
-fit <- lmFit(v, design)
-fit <- eBayes(fit)
+  #time=as.factor(c(rep(time,each=3)))
+  design <- model.matrix(~0+time)
+  dge <- DGEList(counts=counts)
+  dge <- calcNormFactors(dge)
+  
+  v <- voom(dge,design,plot=TRUE)
+  fit <- lmFit(v, design)
+  fit <- eBayes(fit)
 
 
-total<-vector('character')   #for unique genes
-geneMap<-matrix(data=0,nrow=9,ncol=9)
-rownames(geneMap)<-paste("time",c(1:9),sep="")
-colnames(geneMap)<-paste("time",c(1:9),sep="")
+  total<-vector('character')   #for unique genes
+  geneMap<-matrix(data=0,nrow=numberOfPoints,ncol=numberOfPoints)
+  rownames(geneMap)<-paste("time",c(1:numberOfPoints),sep="")
+  colnames(geneMap)<-paste("time",c(1:numberOfPoints),sep="")
 
 
-for (i in c(1:9) ){
-  for (j in c(1:9)){
-    if(i==j){
-      geneMap[paste("time",i,sep=""),paste("time",j,sep="")]<-0
-      next
+  for (i in c(1:numberOfPoints) ){
+    for (j in c(1:numberOfPoints)){
+      if(i==j){
+        geneMap[paste("time",i,sep=""),paste("time",j,sep="")]<-0
+        next
+      }
+      
+      cont.matrix <- makeContrasts(time1-time2, levels=design)
+      a = c(rep(0,each=numberOfPoints))
+      a<-replace(a,c(i,j),c(1,-1))
+      cont.matrix[,1]<-a
+      fit2 <- contrasts.fit(fit, cont.matrix)
+      fit2 <- eBayes(fit2)
+      
+      top<-as.data.frame(topTable(fit2,  number=Inf, sort.by="P",adjust="BH"))
+      top<- top[top$adj.P.Val < 0.05,]
+      topUp<- top[top$logFC > 2,]
+      topDown<- top[top$logFC < -2,]
+      if(!is.null(outputDir)){
+          if(!dir.exists(file.path(outputDir))){
+            dir.create(file.path(mainDir, subDir), showWarnings = FALSE)
+          }
+          write.csv(rownames(topUp),paste(outputDir,"/limma",subject,"-",track,i,"-",j,"up.csv",sep=""),row.names=FALSE)
+          write.csv(rownames(topDown),paste(outputDir,"/limma",subject,"-",track,i,"-",j,"down.csv",sep=""),row.names=FALSE)
+      }
+      # only counting the Up genes as given the reflection, as it runs throug the second half of the (i,j) matrix, it'll switch and the up's will be down
+      # this is preventing changes being counted twice.
+      total<-c(total,rownames(topUp))
+      if (is.integer(nrow(topUp))){
+          geneMap[paste("time",i,sep=""),paste("time",j,sep="")]<-nrow(topUp)
+      }
     }
-    cont.matrix <- makeContrasts(
-      assign("contrast",paste("s",i,"-s",j,sep="")),
-      levels=design
-    )
-    a = c(0,0,0,0,0,0,0,0,0)
-    a<-replace(a,c(i,j),c(1,-1))
-    cont.matrix[,1]<-a
-    fit2 <- contrasts.fit(fit, cont.matrix)
-    fit2 <- eBayes(fit2)
-    
-    top<-as.data.frame(topTable(fit2,  number=Inf, sort.by="P",adjust="BH"))
-    top<- top[top$adj.P.Val < 0.05,]
-    topUp<- top[top$logFC > 2,]
-    topDown<- top[top$logFC < -2,]
-    
-    write.csv(rownames(topUp),paste("/home/ben/workspace/timeCourse/data/deResults/limma/limma",subject,i,"-",j,"up.csv",sep=""),row.names=FALSE)
-    write.csv(rownames(topDown),paste("/home/ben/workspace/timeCourse/data/deResults/limma/limma",subject,i,"-",j,"down.csv",sep=""),row.names=FALSE)
-    # only counting the Up genes as given the reflection, as it runs throug the second half of the (i,j) matrix, it'll switch and the up's will be down
-    # this is preventing changes being counted twice.
-    total<-c(total,rownames(topUp))
-    if (is.integer(nrow(topUp))){
-        geneMap[paste("time",i,sep=""),paste("time",j,sep="")]<-nrow(topUp)
-    }
-    
   }
-}
 
 
 
 ####
+  if(!is.null(outputImages)){
+    if(subject=="plant" && track=="control"){
+      filenameUp = paste(outputImages,"limmaGeneMapControlUp",time1,"-",time2,".png",sep="")
+      filenameDown = paste(outputImages,"limmaGeneMapControlDown",time1,"-",time2,".png",sep="")
+      mainUp<-paste("Actinidia, control - up",sep="")
+      mainDown<-paste("Actinidia, control - down",sep="")
+      palette<-brewer.pal(9,name="Greens")
+    }else if (subject=="plant" && track=="psa"){
+      filenameUp = paste(outputImages,"limmaGeneMapInnoculatedUp",time1,"-",time2,".png",sep="")
+      filenameDown = paste(outputImages,"limmaGeneMapInnoculatedDown",time1,"-",time2,".png",sep="")
+      mainUp<-paste("Actinidia, innoculated - up",sep="")
+      mainDown<-paste("Actinidia, innoculated - down",sep="")
+      palette<-brewer.pal(9,name ="Blues")
+    }else if (subject=="bacteria" && track=="psa"){
+      filenameUp = paste(outputImages,"limmaGeneMapPsaUp",time1,"-",time2,".png",sep="")
+      filenameDown = paste(outputImages,"limmaGeneMapPsaDown",time1,"-",time2,".png",sep="")
+      mainUp<-paste("Psa, bacterial genes - up",sep="")
+      mainDown<-paste("Psa, bacterial genes - down",sep="")
+      palette<-brewer.pal(9,name="Reds")
+    }else{
+      print("there should have been an error before here but either the plant/bacteria or psa/control thing hasn't been specified")
+    }
+  
+    colnames(geneMap)<-timeList
+    rownames(geneMap)<-timeList
 
+    matUp<-geneMap
+    matUp[upper.tri(geneMap)]<-0
+    png(filename=filenameUp,width=800)
+    heatmap.2( log(matUp+1),scale="none",col = palette,margins = c(5, 15),trace = "none",lhei = c(0.75,3),xlab = "Hours post innoculation", na.color="white", main = mainUp ,Colv = FALSE,Rowv =FALSE, dendrogram = "none")
+    dev.off()
 
-if(subject=="plant" && track=="control"){
-  filenameUp = paste(outputDir,"limmaGeneMapControlUp.png",sep="")
-  filenameDown = paste(outputDir,"limmaGeneMapControlDown.png",sep="")
-  mainUp<-paste("Actinidia, control - up",sep="")
-  mainDown<-paste("Actinidia, control - down",sep="")
-  palette<-brewer.pal(9,name="Greens")
-}else if (subject=="plant" && track=="psa"){
-  filenameUp = paste(outputDir,"limmaGeneMapInnoculatedUp.png",sep="")
-  filenameDown = paste(outputDir,"limmaGeneMapInnoculatedDown.png",sep="")
-  mainUp<-paste("Actinidia, innoculated - up",sep="")
-  mainDown<-paste("Actinidia, innoculated - down",sep="")
-  palette<-brewer.pal(9,name ="Blues")
-}else if (subject=="bacteria" && track=="psa"){
-  filenameUp = paste(outputDir,"limmaGeneMapPsaUp.png",sep="")
-  filenameDown = paste(outputDir,"limmaGeneMapPsaDown.png",sep="")
-  mainUp<-paste("Psa, bacterial genes - up",sep="")
-  mainDown<-paste("Psa, bacterial genes - down",sep="")
-  palette<-brewer.pal(9,name="Reds")
-}else{
-  print("there should have been an error before here but either the plant/bacteria or psa/control thing hasn't been specified")
+    matDown<-geneMap
+    matDown[lower.tri(geneMap)]<-0
+    png(filename=filenameDown,width=800)
+    heatmap.2( log(matDown+1),scale="none",col = palette,margins = c(5, 15),trace = "none",lhei = c(0.75,3),xlab = "Hours post innoculation", na.color="white", main = mainDown,Colv = FALSE,Rowv =FALSE, dendrogram = "none")
+    dev.off()
+  }
+    print("total number of gene changes:")
+    print(length(total))
+    print("total number of unique genes changing:")
+    print(length(unique(total)))
+
+    par(mfrow=c(1,1))
+  heatmap.2( log(matDown+1),scale="none",col = palette,margins = c(5, 15),trace = "none",lhei = c(0.75,3),xlab = "Hours post innoculation", na.color="white", main = mainDown,Colv = FALSE,Rowv =FALSE, dendrogram = "none")
+
 }
-
-colnames(geneMap)<-c(1.5,3,6,12,24,48,72,96,120)
-rownames(geneMap)<-c(1.5,3,6,12,24,48,72,96,120)
-
-matUp<-geneMap
-matUp[upper.tri(geneMap)]<-0
-png(filename=filenameUp,width=800)
-heatmap.2( log(matUp+1),scale="none",col = palette,margins = c(5, 15),trace = "none",lhei = c(0.75,3),xlab = "Comparison", na.color="white", main = mainUp ,Colv = FALSE,Rowv =FALSE, dendrogram = "none")
-dev.off()
-
-
-#heatmap.2( log(matUp+1),scale="none",col = palette,margins = c(5, 15),trace = "none",lhei = c(0.75,3),xlab = "Comparison", na.color="white", main = mainUp ,Colv = FALSE,Rowv =FALSE, dendrogram = "none")
-
-matDown<-geneMap
-matDown[lower.tri(geneMap)]<-0
-png(filename=filenameDown,width=800)
-heatmap.2( log(matDown+1),scale="none",col = palette,margins = c(5, 15),trace = "none",lhei = c(0.75,3),xlab = "Comparison", na.color="white", main = mainDown,Colv = FALSE,Rowv =FALSE, dendrogram = "none")
-dev.off()
-
-
-print("total number of gene changes:")
-print(length(total))
-print("total number of unique genes changing:")
-print(length(unique(total)))
-
-par(mfrow=c(1,1))
-heatmap.2( log(matDown+1),scale="none",col = palette,margins = c(5, 15),trace = "none",lhei = c(0.75,3),xlab = "Comparison", na.color="white", main = mainDown,Colv = FALSE,Rowv =FALSE, dendrogram = "none")
-
-
 
 
 
